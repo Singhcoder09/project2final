c5a04ca255cf5155fbde20de1d330cb057ff9a38
Gagandeep Singh <gagandeepssingh1992@gmail.com>
2021-04-27 05:13:14 -0700

    firstcommit

---
+++ Final Project/models/Registration.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const mySchema = new mongoose.Schema ({
+    name: {
+        type: String,
+        trim: true,
+    },
+    email: {
+        type: String,
+        trim: true,
+    },
+    username: {
+        type: String,
+        trim: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        trim: true,
+    },
+    });
+
+module.exports = mongoose.model('Registration', mySchema);
